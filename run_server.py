f3b1c5ca1a1c7b903e431d1ef7b2988334f03b4a
Keeyon Ebrahimi <keeyon2@gmail.com>
2015-04-30 16:21:04 -0400

    Merging front end

--- run_server.py
+++ run_server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from gensim import models
 import numpy as np
 from operator import itemgetter
@@ -27,23 +28,44 @@ def get_similarities(m1, m2, m3, word, w_n):
 		    
 
 
-@app.route('/<word_name>')
-def load_models(word_name):
-	
-	x = tuple(mod12.most_similar(word_name))
-	y = tuple(mod13.most_similar(word_name))
-	z = tuple(mod14.most_similar(word_name))
-	words = []
-	words = get_words(x, words) 
-	words = get_words(y, words)
-	words = get_words(z, words)
-	similars = get_similarities(mod12, mod13, mod14, words, word_name)
-	varians = map(np.var, similars)
-	pairs = zip(words, similars, varians)	
+# @app.route('/<word_name>')
+# def load_models(word_name):
+# 	
+# 	x = tuple(mod12.most_similar(word_name))
+# 	y = tuple(mod13.most_similar(word_name))
+# 	z = tuple(mod14.most_similar(word_name))
+# 	words = []
+# 	words = get_words(x, words) 
+# 	words = get_words(y, words)
+# 	words = get_words(z, words)
+# 	similars = get_similarities(mod12, mod13, mod14, words, word_name)
+# 	varians = map(np.var, similars)
+# 	pairs = zip(words, similars, varians)	
+# 
+# 	pairs.sort(key=itemgetter(2), reverse=True)
+# 	for i,j,k in pairs:
+# 		print i, j, k 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-	pairs.sort(key=itemgetter(2), reverse=True)
-	for i,j,k in pairs:
-		print i, j, k 
+@app.route('/', methods=['POST'])
+def load_models():
+    word_name = request.form['interestingtextfield']
+    x = tuple(mod12.most_similar(word_name))
+    y = tuple(mod13.most_similar(word_name))
+    z = tuple(mod14.most_similar(word_name))
+    words = []
+    words = get_words(x, words) 
+    words = get_words(y, words)
+    words = get_words(z, words)
+    similars = get_similarities(mod12, mod13, mod14, words, word_name)
+    varians = map(np.var, similars)
+    pairs = zip(words, similars, varians)	
+
+    pairs.sort(key=itemgetter(2), reverse=True)
+    for i,j,k in pairs:
+        print i, j, k 
 
 mod12 = models.Word2Vec.load('model2012')
 mod13 = models.Word2Vec.load('model2013')
